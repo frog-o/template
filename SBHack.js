356ed8245adea189cd5c7de874857a16e9539631
frog-o <47947111+frog-o@users.noreply.github.com>
2023-07-29 12:21:10 -0400

    still trying to fixstackblitz bug

--- SBHack.js
+++ SBHack.js
@@ -1,7 +1,9 @@
 #!/usr/local/bin/node
-import {execSync as exec} from 'child_process');
+import { execSync as exec } from 'child_process';
+import { existsSync } from 'fs';
 let userEmail = '47947111+frog-o@users.noreply.github.com';
-
-exec('git config user.email --global "' + userEmail + '"');
-console.log("Ran ok")
-exec('yarn intsall')
+if (existsSync('/usr/bin/git')) {
+  exec('git config user.email --global "' + userEmail + '"');
+  console.log('Ran ok');
+}
+exec('yarn install');
