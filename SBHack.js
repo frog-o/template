c36946da86a369aab109549c3d9619d3c8e4e7e3
frog-o <47947111+frog-o@users.noreply.github.com>
2023-07-29 21:18:50 -0400

    working

--- SBHack.js
+++ SBHack.js
@@ -3,7 +3,9 @@ import { execSync as exec } from 'child_process';
 import { existsSync } from 'fs';
 let userEmail = '47947111+frog-o@users.noreply.github.com';
 if (existsSync('/usr/bin/git')) {
-  exec('git config user.email --global "' + userEmail + '"');
+  exec('git config user.email --global\ "' + userEmail + '"');
+  exec('git config user.email "' + userEmail + '"');
+  
   console.log('Ran ok');
 }
 exec('yarn install');
